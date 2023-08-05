c30e1b6435f52ed9142fb1453ffc39b3c0a143ff
Anastasia Buglakova <bugla2005@mail.ru>
2023-08-05 13:34:49 +0300

    HW2 is done

--- HW2main.py
+++ HW2main.py
@@ -1,13 +1,39 @@
-# This is a sample Python script.
+# 1 Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое
+# представление. Функцию hex используйте для проверки своего результата.
+from math import gcd
 
-# Press ⌃R to execute it
+# inputed_number = int(input("Введите целое число: "))
+# check = inputed_number
+# print(inputed_number)
+# res = ''
+# st = '0123456789abcdef'
+#
+# while inputed_number > 0:
+#     res = st[inputed_number % 16] + res
+#     inputed_number //= 16
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# print(res)
+# print('0x'+res == hex(check))
 
-fuflo = 'd'
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# 2 Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
+# Программа должна возвращать сумму и произведение* дробей.
+# Для проверки своего кода используйте модуль fractions.
 
+import fractions
+
+n1, n2 = input('Введте первое дробное число через /: '), input('Введте второе дробное число через /: ')
+
+mult = 1
+n1_1, n1_2 = int(n1.split('/')[0]), int(n1.split('/')[1])
+n2_1, n2_2 = int(n2.split('/')[0]), int(n2.split('/')[1])
+
+common_del_summ = gcd(n1_1 * n2_2 + n2_1 * n1_2, n1_2 * n2_2)
+summ = str(int((n1_1 * n2_2 + n2_1 * n1_2) / common_del_summ)) + '/' + str(int((n1_2 * n2_2) / common_del_summ))
+
+common_del_mult = common_del_summ = gcd(n1_1 * n2_1, n1_2 * n2_2)
+mult = str(int(n1_1 * n2_1/common_del_mult)) + '/' + str(int(n1_2 * n2_2 / common_del_mult))
+print(summ, mult)
+
+summ_check = fractions.Fraction(n1_1, n1_2) + fractions.Fraction(n2_1, n2_2)
+mult_check = fractions.Fraction(n1_1, n1_2) * fractions.Fraction(n2_1, n2_2)
+print(summ_check, mult_check)
