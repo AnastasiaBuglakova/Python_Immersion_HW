38f59b6dcbc08f915f6e4132f924c157569c941e
Anastasia Buglakova <bugla2005@mail.ru>
2023-08-05 14:18:14 +0300

    HW2 is corrected

--- HW2main.py
+++ HW2main.py
@@ -4,16 +4,16 @@ from math import gcd
 
 # inputed_number = int(input("Введите целое число: "))
 # check = inputed_number
-# print(inputed_number)
+#
 # res = ''
 # st = '0123456789abcdef'
 #
 # while inputed_number > 0:
 #     res = st[inputed_number % 16] + res
 #     inputed_number //= 16
-
-# print(res)
-# print('0x'+res == hex(check))
+#
+# print(f'Ваше число в 16м представлении = {res}')
+# print('Проверка', '0x'+res == hex(check))
 
 # 2 Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
 # Программа должна возвращать сумму и произведение* дробей.
@@ -28,12 +28,19 @@ n1_1, n1_2 = int(n1.split('/')[0]), int(n1.split('/')[1])
 n2_1, n2_2 = int(n2.split('/')[0]), int(n2.split('/')[1])
 
 common_del_summ = gcd(n1_1 * n2_2 + n2_1 * n1_2, n1_2 * n2_2)
-summ = str(int((n1_1 * n2_2 + n2_1 * n1_2) / common_del_summ)) + '/' + str(int((n1_2 * n2_2) / common_del_summ))
+if int((n1_2 * n2_2) / common_del_summ) ==1:
+    summ = str(int((n1_1 * n2_2 + n2_1 * n1_2) / common_del_summ))
+else:
+    summ = str(int((n1_1 * n2_2 + n2_1 * n1_2) / common_del_summ)) + '/' + str(int((n1_2 * n2_2) / common_del_summ))
 
 common_del_mult = common_del_summ = gcd(n1_1 * n2_1, n1_2 * n2_2)
-mult = str(int(n1_1 * n2_1/common_del_mult)) + '/' + str(int(n1_2 * n2_2 / common_del_mult))
-print(summ, mult)
+if int(n1_2 * n2_2 / common_del_mult) == 1:
+    mult = str(int(n1_1 * n2_1 / common_del_mult))
+else:
+    mult = str(int(n1_1 * n2_1 / common_del_mult)) + '/' + str(int(n1_2 * n2_2 / common_del_mult))
+print('сумма чисел =', summ, ', их произведение =', mult)
 
 summ_check = fractions.Fraction(n1_1, n1_2) + fractions.Fraction(n2_1, n2_2)
 mult_check = fractions.Fraction(n1_1, n1_2) * fractions.Fraction(n2_1, n2_2)
-print(summ_check, mult_check)
+
+print('Проверка:', summ == str(summ_check), ',', mult == str(mult_check))
